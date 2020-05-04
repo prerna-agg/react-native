2deb4aef0d583095b667044363a5a2f67293921f
prerna <paruprerna@gmail.com>
2020-05-04 20:24:09 +0530

    components part one

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your appsss!</Text>
-		</View>
-	);
+	return <Main />;
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
