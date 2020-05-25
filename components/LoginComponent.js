b2236496386eafe33e89a6c1aa949a25fd73972a
prerna <paruprerna@gmail.com>
2020-05-25 19:45:35 +0530

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -8,6 +8,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { baseUrl } from '../shared/baseUrl';
 import { NavigationContainer } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+import { Asset } from 'expo-asset';
 
 class LoginTab extends Component {
 	constructor(props) {
@@ -154,9 +156,19 @@ class RegisterTab extends Component {
 			});
 			if (!capturedImage.cancelled) {
 				console.log(capturedImage);
-				this.setState({ imageUrl: capturedImage.uri });
+				this.processImage(capturedImage.uri);
 			}
 		}
+	};
+
+	processImage = async (imageUri) => {
+		let processedImage = await ImageManipulator.manipulateAsync(
+			imageUri,
+			[{ resize: { width: 400 } }],
+			{ format: 'png' }
+		);
+		console.log(processedImage);
+		this.setState({ imageUrl: processedImage.uri });
 	};
 
 	static navigationOptions = {
